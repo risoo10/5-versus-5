f81e79264e97f098c0e3a2c0b45fbe64dee190b6
Richard Mocak <richard.mocak@gmail.com>
2019-06-06 21:59:43 +0200

    Initalize angular project.

---
+++ five-versus-five/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fvf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'five-versus-five';
+}
