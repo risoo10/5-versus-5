2979e662cd1e1bf425dd1ff838c3ac1d7f316acb
Richard Mocak <richard.mocak@gmail.com>
2019-06-08 11:41:30 +0200

    Styling for question page. Edited styling of intro.

--- five-versus-five/src/app/app.module.ts
+++ five-versus-five/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { IntroComponent } from './pages/intro/intro.component';
 import { QuestionComponent } from './pages/question/question.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent, IntroComponent, QuestionComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatIconModule],
   providers: [HorizonService],
   bootstrap: [AppComponent],
 })
